f3d371287d27e1172a37c8e77e5ae9f282b3f139
easai <aksarih@gmail.com>
2021-10-30 22:52:40 -0400

    Updated doc/conf.py path setting.

--- doc/conf.py
+++ doc/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, 'C:\\prog-py\\knapsack\\src\\knapsack')
+sys.path.insert(0, '../src/knapsack')
 
 
 # -- Project information -----------------------------------------------------
